a977d7ab147cc5af125b168cb3c197db44550518
Pink-in <Sittipol.tum@gmail.com>
2025-09-18 00:36:23 +0700

    Create scope_that.py

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,9 @@
+def add_one(x):
+    x += 1
+    return x
+
+if __name__ == "__main__":
+    num = 5
+    print("Before calling add_one:", num)
+    num = add_one(num)
+    print("After calling add_one:", num)
