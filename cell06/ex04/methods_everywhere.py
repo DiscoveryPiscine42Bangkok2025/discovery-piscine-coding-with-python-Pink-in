846d5b7af72b46725edca42d3c333832cff18278
Pink-in <Sittipol.tum@gmail.com>
2025-09-18 00:35:56 +0700

    Create methods_everywhere.py

---
+++ cell06/ex04/methods_everywhere.py
@@ -0,0 +1,22 @@
+def shrink(s):
+    print(s[:8])
+
+def enlarge(s):
+    if len(s) < 8:
+        print(s + 'Z' * (8 - len(s)))
+    else:
+        print(s)
+
+if __name__ == "__main__":
+
+    import sys
+    if len(sys.argv) < 2:
+        print("none")
+    else:
+        for param in sys.argv[1:]:
+            if len(param) > 8:
+                shrink(param)
+            elif len(param) < 8:
+                enlarge(param)
+            else:
+                print(param)
