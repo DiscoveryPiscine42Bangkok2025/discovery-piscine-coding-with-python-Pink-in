1df3fd6e075ff474f0cd6a42844fc31be35da37d
Pink-in <Sittipol.tum@gmail.com>
2025-09-17 21:22:01 +0700

    Create age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,8 @@
+agei=input("Please tell me your age: ")
+print(f"You are currently {agei} years old.")
+age=int(agei)
+i=1
+while i<=3:
+ print(f"In {i*10} years, you'll be {age+(10*i)} years old.")
+ i+=1
+
